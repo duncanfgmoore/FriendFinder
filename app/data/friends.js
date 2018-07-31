d4d8048a35f5805630fdc08dbace902abd313af5
Duncan <duncan@Duncans-MacBook-Pro.local>
2018-07-31 13:47:18 -0500

    files connected

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+{
+    "name": "Mike",
+    "photo": "photo",
+    "scores": [
+        3,
+        4,
+        2,
+        5,
+        4,
+        2,
+        1,
+        4,
+        1,
+        3
+    ]
+},
+{
+    "name": "Stacey",
+    "photo": "photo",
+    "scores": [
+        1,
+        3,
+        5,
+        3,
+        2,
+        4,
+        1,
+        1,
+        4,
+        4
+    ]
+},
+{
+    "name": "Bill",
+    "photo": "photo",
+    "scores": [
+        3,
+        5,
+        5,
+        5,
+        2,
+        2,
+        3,
+        4,
+        1,
+        5
+    ]
+}
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,38 @@
+
+var friends = require("../data/friends.js");
+
+module.exports = function(app) {
+//get function to retreive information about the chosen friend
+app.get("/api/friends", function(req, res) {
+    
+   
+   return res.json(friends);
+    
+  });
+
+
+
+  //put function to input your information entered as a possible new friend
+app.post("/api/friends", function(req, res) {
+    
+var totalDifference = 0;
+
+var yourMatch = {};
+
+//for loop to go through all the friends in the array 
+for (var i = 0; i < friends.length; i++) {
+
+    console.log(friends[i]);
+}
+//for loop to go through each one of the answers and compare them
+    for (var i = 0; i < scores.length; i++) {
+
+        var questionDifference = Math.ad(yourInfo.score[i] - friends.scores[i]);
+
+        totalDifference = totalDifference + questionDifference
+
+
+    }
+
+  });
+};
