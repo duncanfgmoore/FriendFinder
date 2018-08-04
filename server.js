72cc09d20c89624b85a5899f02ec636681383b64
Duncan <duncan@er-10-129-198-102.wireless.umn.edu>
2018-08-04 14:32:23 -0500

    round 3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
