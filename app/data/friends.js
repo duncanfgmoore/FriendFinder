1a622c7537c5208b9685090c4b19bc071851e660
duncanfgmoore <duncanfgmoore@gmail.com>
2019-01-06 22:52:42 -0600

    formatting change

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,85 +1,33 @@
 var friends = [
-{
-    "name": "Mike",
-    "photo": "https://www.findaqualityman.com/wp-content/uploads/2013/08/bigstock-Close-Up-Portrait-Of-Man-Stand-27379967.jpg",
-    "scores": [
-        3,
-        4,
-        2,
-        5,
-        4,
-        2,
-        1,
-        4,
-        1,
-        3
-    ]
-
-},
-{
-    "name": "Stacey",
-    "photo": "https://srkheadshotday.com/wp-content/uploads/Lisa_Masten_Headshot_17B2797_Crop32-1024x683.jpg",
-    "scores": [
-        1,
-        3,
-        5,
-        3,
-        2,
-        4,
-        1,
-        1,
-        4,
-        4
-    ]
-},
-{
-    "name": "Bill",
-    "photo": "http://media.beliefnet.com/~/media/photos-with-attribution/inspiration/002/peoplemiddleagedmansmilingjpg.jpg",
-    "scores": [
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5
-    ]
-},
-{
-    "name": "Mary",
-    "photo": "http://michaelhelms.com/wp-content/uploads/2012/04/Akia4078.jpg",
-    "scores": [
-        3,
-        5,
-        3,
-        4,
-        1,
-        1,
-        2,
-        4,
-        5,
-        5
-    ]
-},
-{
-    "name": "Robert",
-    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Mikhail_Men.jpg/440px-Mikhail_Men.jpg",
-    "scores": [
-        2,
-        4,
-        5,
-        1,
-        3,
-        4,
-        4,
-        2,
-        4,
-        5
-    ]
-}
+  {
+    name: "Mike",
+    photo:
+      "https://www.findaqualityman.com/wp-content/uploads/2013/08/bigstock-Close-Up-Portrait-Of-Man-Stand-27379967.jpg",
+    scores: [3, 4, 2, 5, 4, 2, 1, 4, 1, 3]
+  },
+  {
+    name: "Stacey",
+    photo:
+      "https://srkheadshotday.com/wp-content/uploads/Lisa_Masten_Headshot_17B2797_Crop32-1024x683.jpg",
+    scores: [1, 3, 5, 3, 2, 4, 1, 1, 4, 4]
+  },
+  {
+    name: "Bill",
+    photo:
+      "http://media.beliefnet.com/~/media/photos-with-attribution/inspiration/002/peoplemiddleagedmansmilingjpg.jpg",
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  },
+  {
+    name: "Mary",
+    photo: "http://michaelhelms.com/wp-content/uploads/2012/04/Akia4078.jpg",
+    scores: [3, 5, 3, 4, 1, 1, 2, 4, 5, 5]
+  },
+  {
+    name: "Robert",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Mikhail_Men.jpg/440px-Mikhail_Men.jpg",
+    scores: [2, 4, 5, 1, 3, 4, 4, 2, 4, 5]
+  }
 ];
 
 module.exports = friends;
