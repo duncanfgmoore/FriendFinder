b427287457dd1b142813dc25f876e34926000a72
duncanfgmoore <duncanfgmoore@gmail.com>
2019-01-06 22:53:05 -0600

    removing code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,49 +10,5 @@ module.exports = function (app) {
 
     });
 
-    //put function to input your information entered as a possible new friend
-    // app.post("/api/friends", function (req, res) {
-
-    //     var totalDifference = 0;
-
-    //     var scoresArray = [];
-
-    //     var userInfo = req.body;
-
-    //     var winningName = "Nobody";
-    //     var winningScore = 300;
-
-    //     for (var i = 0; i < friends.length; i++) {
-
-    //         //console.log(friends[i].name);
-    //         totalDifference = 0
-
-    //         for (var j = 0; j < 10; j++) {
-
-    //             var questionDifference = Math.abs(parseInt(userInfo.scores[j]) - parseInt(friends[i].scores[j]));
-
-    //             totalDifference += questionDifference
-    //         }
-
-    //         scoresArray.push(totalDifference)
-
-
-    //         if (totalDifference < winningScore) {
-    //             winningScore = totalDifference;
-    //             winningName = friends[i].name;
-    //         }
-
-
-
-    //     }
-    //     console.log(scoresArray[0] + "  " + scoresArray[1] + "  " + scoresArray[2])
-    //     console.log(winningName)
-    //     console.log(winningScore);
-        
-       
-
-    //     console.log("Your new best friend is " + winningName)
-
-
-    // });
+    
 };
